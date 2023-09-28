da9ef2d85fb16e158e25f4e5af1c0e4d06794c82
houseyuri <houseyuri@gmail.com>
2023-09-28 14:22:15 -0400

    added files

---
+++ W3_game.py
@@ -0,0 +1,110 @@
+# Declare Answers
+def answer(name):
+    if name == "queens":
+        print("QUEENS is for you!")
+        print("🌙｀、、｀ヽ｀ヽ｀、、ヽヽ、｀、ヽ｀ヽ｀ヽ｀")
+        print("ヽ｀、｀ヽ｀、ヽ｀｀、ヽ｀ヽ｀、ヽヽ｀ヽ、ヽ")
+        print("｀ヽ、ヽヽ｀ヽ｀、｀｀ヽ｀ヽ、ヽ、ヽ｀ヽ｀ヽ")
+        print("ヽ｀ヽ｀、ヽヽ｀｀、ヽ｀、ヽヽ 🚶‍♀ ヽ｀｀｀｀")
+
+    if name == "brooklyn":
+        print("You belong in BROOKLYN!")
+        print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀")
+        print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀")
+        print("⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀")
+        print("⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣻⣿⡿⣿⣿⣿⣿⣿⣷⡀⠀⠀")
+        print("⠀⠀⠀⠀⠀⠀⣴⢫⣿⣿⣿⣿⢫⣿⣿⡇⠹⣿⣿⣿⣿⣿⣧⠀⠀")
+        print("⠀⠀⠀⠀⠀⠀⠏⢸⣿⣿⣿⣿⣾⣟⣛⠛⠃⠘⣿⣿⣿⣿⣿⠀⠀")
+        print("⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣧⣈⠱⠀⠀⢸⣿⣿⣿⠏⠀⠀")
+        print("⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⡏⠙⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+        print("⠀⠀⠀⠀⢀⣀⣿⣿⣿⣿⣿⣿⣿⣦⣀⡿⠂⠀⠀⠀⠀⠀⠀⠀⠀")
+        print("⠀⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠐⠄⠀⠀⠀⠀⠀⠀⠀")
+        print("⢸⣿⣿⣿⣿⡏⠙⠛⠿⠟⠛⠛⠋⠉⠀⠀⠀⢸⣶⣷⣶⣦⣤⣀⠀")
+        print("⠈⣺⣿⡏⢿⣷⡄⠰⣄⡀⠀⠀⠀⠀⠀⢀⣶⣿⡟⣿⣿⣿⣿⣻⡇")
+        print("⠀⠿⠿⠷⠸⠿⠷⠄⠈⠿⠦⠤⠴⠾⠀⠸⠿⠿⠀⠹⠿⠿⠿⠆⠃")
+
+    if name == "manhattan":
+        print("You are a MANHATTAN kid!")
+        print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⢀⣀⡀⠀⠀")
+        print("⠀⠀⣀⠀⢀⡀⠀⣀⡀⣀⣀⣀⣀⠀⣬⣽⣿⣿⣦⣴⣿⣿⣿⡆⠀")
+        print("⠀⠈⣿⡆⣿⣿⢰⣿⠃⣿⣟⣛⡛⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀")
+        print("⠀⠀⢻⣷⡿⢻⣾⡟⠀⣿⣟⣛⣃⠀⠈⢿⣿⣿⣿⣿⣿⣿⡿⠁⠀")
+        print("⠀⠀⢈⡛⠃⢘⡛⣁⡀⠛⢛⣛⠛⣀⣀⡀⠙⢿⣿⣿⡿⠋⠀⠀⠀")
+        print("⠀⠀⣿⣿⣆⣿⡇⠙⣿⣤⣿⠏⣾⡟⠛⠿⠆⠀⠉⠁⠀⠀⠀⠀⠀")
+        print("⠀⠀⣿⡏⢿⣿⡇⠀⢸⣿⡇⠘⣿⣆⢀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀")
+        print("⠀⠀⠛⠃⠈⠛⠃⠀⠈⠛⠁⠀⠈⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+
+    if name == "newjersey":
+        print("Boooooo.. Get on the PATH to Jersey..")
+        print("N")
+        print("　 O")
+        print("　　　 O")
+        print("　　　　 o")
+        print("　　　　　o")
+        print("　　　　　 o")
+        print("　　　　　o")
+        print("　　　　 。")
+        print("　　　 。")
+        print("　　　.")
+        print("　　　.")
+        print("　　　 .")
+        print("　　　　.")
+
+
+# Declare questions
+def question1():
+    while True:
+        print("Q1 _ What is your favorite food?")
+        print("1. Ramen")
+        print("2. Italian")
+        print("3. Chinese Food")
+        print("\n")
+        choice1 = input("Select a number: ")
+        if choice1 in ["1", "2", "3"]:
+            return choice1
+        else:
+            print("Enter a valid number\n")
+
+
+def question2():
+    while True:
+        print("Q2 _ What do you usually do in your free time?")
+        print("1. Spend time alone")
+        print("2. Go out and party all night")
+        print("3. Chill with friends and family")
+        print("\n")
+        choice2 = input("Select a number: ")
+        if choice2 in ["1", "2", "3"]:
+            return choice2
+        else:
+            print("Enter a valid number\n")
+
+
+# Game Start
+name = input("What is your name? >>> ")
+
+print("\n")
+print(f"Hello, {name}! Let's find out which borough is for you.")
+print("\n")
+
+choice1 = question1()
+
+choice2 = question2()
+
+# Ending
+if choice1 == "1" and choice2 == "1":
+    answer("queens")
+elif choice1 == "1" and choice2 == "2":
+    answer("manhattan")
+elif choice1 == "1" and choice2 == "3":
+    answer("brooklyn")
+elif choice1 == "2" and choice2 == "1":
+    answer("manhattan")
+elif choice1 == "2" and (choice2 == "2" or "3"):
+    answer("newjersey")
+elif choice1 == "3" and choice2 == "1":
+    answer("queens")
+elif choice1 == "3" and choice2 == "2":
+    answer("manhattan")
+else:
+    answer("brooklyn")
